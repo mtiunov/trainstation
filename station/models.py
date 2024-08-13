718a93c0eb6606db2fc0de3ee1fdc144dfecc616
Mikhail <tiunovmixs@gmail.com>
2024-08-13 20:20:06 +0300

    Added core models, user model, and settings. Overrode user model, created superuser, added models to admin panel.

--- station/models.py
+++ station/models.py
@@ -1,3 +1,104 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(TrainType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, related_name="source_routes", on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Station, related_name="destination_routes", on_delete=models.CASCADE
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source} to {self.destination}"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["route"]
+
+    def __str__(self):
+        return self.route.name + " " + self.train.name
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        Journey, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+
+    def clean(self):
+        if self.cargo < 0:
+            raise ValidationError("Cargo must be a non-negative integer.")
+
+        if self.seat < 1:
+            raise ValidationError("Seat number must be within the valid range.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Ticket, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Journey: {self.journey}, Seat: {self.seat}, Cargo: {self.cargo}"
